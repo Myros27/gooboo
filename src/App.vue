bff12fd83feda86b9f041c80b3f742daadd752a8
Severin <severinw27@gmail.com>
2024-10-25 07:39:54 +0200

    better screen reader

--- src/App.vue
+++ src/App.vue
@@ -370,17 +370,17 @@
           <div>{{ item }}</div>
         </div>
       </gb-tooltip>
-      <gb-tooltip v-if="canSeeHourglass" key="ancient-hourglass" :title-text="$vuetify.lang.t('$vuetify.hourglass.title')">
+      <gb-tooltip v-if="canSeeHourglass" aria-label="Ancient hourglass 1" key="ancient-hourglass" :title-text="$vuetify.lang.t('$vuetify.hourglass.title')">
         <template v-slot:activator="{ on, attrs }">
-          <div aria-label="Ancient hourglass" class="hourglass-container mx-2" @click="openDustDialog" v-bind="attrs" v-on="on">
-            <v-icon color="secondary" class="hourglass-outline">mdi-timer-sand-full</v-icon>
+          <div aria-label="Ancient hourglass 2" class="hourglass-container mx-2" @click="openDustDialog" v-bind="attrs" v-on="on">
+            <v-icon aria-label="Ancient hourglass 3" color="secondary" class="hourglass-outline">mdi-timer-sand-full</v-icon>
             <div class="hourglass-bg" :style="`top: ${ hourglassShift }px;`">
               <v-icon color="amber" class="hourglass-bg-inner">mdi-timer-sand-full</v-icon>
             </div>
             <v-icon class="hourglass-outline">mdi-timer-sand-empty</v-icon>
           </div>
         </template>
-        <div v-if="isOnMainFeature" class="text-center">{{ $vuetify.lang.t('$vuetify.hourglass.subtitle') }}</div>
+        <div v-if="isOnMainFeature" aria-label="Ancient hourglass" class="text-center">{{ $vuetify.lang.t('$vuetify.hourglass.subtitle') }}</div>
         <div class="d-flex justify-center">
           <currency name="school_goldenDust"></currency>
         </div>
